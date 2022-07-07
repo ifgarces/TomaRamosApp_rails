ec4ed3f7adc19101ec72302ba933b06c04072191
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-07-06 22:10:03 -0400

    #17 HTTPS support: hotfix yet again
    Related to web server SSL configurations

--- TomaRamosWebApp/docker-entrypoint.sh
+++ TomaRamosWebApp/docker-entrypoint.sh
@@ -19,10 +19,14 @@ rake data_importer:csv --trace
 
 # Starting web server, logs Rails stuff to the default `logs/development.log` instead of STDOUT
 if [[ "${SERVE_OVER_HTTPS}" == "true" ]]; then
-    sleep 6 #? (strictly needed?) waiting until files are copied from `restart-rails.sh`
+    #! temporal workaround, must properly use production environment instead!
+    sed -i 's/config.consider_all_requests_local = true/config.consider_all_requests_local = false/' ./TomaRamosWebApp/config/environments/development.rb
+    sed -i 's/# config.force_ssl = true/config.force_ssl = true/' ./TomaRamosWebApp/config/environments/development.rb
+
+    # References: https://stackoverflow.com/a/31939157/12684271
     rails server \
         --using=puma \
-        --binding="ssl://${WEB_SERVER_HOST}:${WEB_SERVER_PORT}?key=/etc/ssl/certs/tomaramos.app.key&cert=/etc/ssl/certs/tomaramos.app-bundle.crt" \
+        --binding="ssl://${WEB_SERVER_HOST}:${WEB_SERVER_PORT}?key=/etc/ssl/certs/tomaramos.app.key&cert=/etc/ssl/certs/tomaramos.app-bundle.crt&verify_mode=none" \
         --environment=development \
         --no-log-to-stdout
 elif [[ "${SERVE_OVER_HTTPS}" == "false" ]]; then
