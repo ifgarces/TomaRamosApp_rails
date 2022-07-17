d5f01cce936551a44e5f9aff60e58c2ebe5cdd61
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-07-17 14:40:03 -0400

    Fix on "mobile notice", and refactorings
    - Now the "mobile notice" is displayed/hidden properly based on window width
    - Deleted unused code
    - Applied JS formatter

--- TomaRamosWebApp/app/javascript/application.js
+++ TomaRamosWebApp/app/javascript/application.js
@@ -1,4 +1,23 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import * as bootstrap from "bootstrap";
+
+let betterInMobileNotice, MIN_SCREEN_WIDTH_PX_FOR_MOBILE_NOTICE;
+
+handleMobileNotice = function () {
+    betterInMobileNotice.hidden = (
+        document.documentElement.clientWidth < MIN_SCREEN_WIDTH_PX_FOR_MOBILE_NOTICE
+    );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    betterInMobileNotice = document.getElementById("BetterInMobileNotice");
+    let mainDiv = document.getElementById("MainDivWhiteLayer");
+    MIN_SCREEN_WIDTH_PX_FOR_MOBILE_NOTICE = mainDiv.offsetWidth + betterInMobileNotice.offsetWidth;
+    handleMobileNotice();
+});
+
+window.onresize = function (event) {
+    handleMobileNotice();
+};
