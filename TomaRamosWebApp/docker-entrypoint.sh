24599dfc53f1d9908a3783b9644aa36bb151381c
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-11-28 13:53:47 -0300

    #17: small fixes for WIP HTTPS support

--- TomaRamosWebApp/docker-entrypoint.sh
+++ TomaRamosWebApp/docker-entrypoint.sh
@@ -21,13 +21,47 @@ rake data_importer:csv --trace
 # Starting web server, logging to STDOUT/STDERR
 if [[ "${SERVE_OVER_HTTPS}" == "true" ]]; then
     #! UGLY temporal workaround, must properly use production environment instead!
-    sed -i 's/config.consider_all_requests_local = true/config.consider_all_requests_local = false/' ./TomaRamosWebApp/config/environments/development.rb
-    sed -i 's/# config.force_ssl = true/config.force_ssl = true/' ./TomaRamosWebApp/config/environments/development.rb
+    sed -i 's/config.consider_all_requests_local = true/config.consider_all_requests_local = false/' ./config/environments/development.rb
+    sed -i 's/# config.force_ssl = true/config.force_ssl = true/' ./config/environments/development.rb
+
+    #! Testing localhost now
+    echo "
+[ req ]
+prompt                 = no
+days                   = 365
+distinguished_name     = req_distinguished_name
+req_extensions         = v3_req
+
+[ req_distinguished_name ]
+countryName            = CL
+stateOrProvinceName    = Metropolitana
+localityName           = Santiago
+organizationName       = tomaramos.app
+organizationalUnitName = ifgarces
+commonName             = localhost
+emailAddress           = hostmaster@tomaramos.app
+
+[ v3_req ]
+basicConstraints       = CA:false
+extendedKeyUsage       = serverAuth
+subjectAltName         = @sans
+
+[ sans ]
+DNS.0 = localhost
+DNS.1 = localhost
+    " >> ca-config.temp.conf
+
+    openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -utf8 \
+        -keyout /etc/ssl/certs/tomaramos.app.key \
+        -out /etc/ssl/certs/tomaramos.app.crt \
+        -config ca-config.temp.conf
+        #"/C=CL/ST=Región Metropolitana/L=Santiago/O=TomaRamosApp/OU=/CN=/emailAddress="
 
     # References: https://stackoverflow.com/a/31939157/12684271
+    echo "::: SERVING OVER HTTPS :::"
     rails server \
         --using=puma \
-        --binding="ssl://${WEB_SERVER_HOST}:${WEB_SERVER_PORT}?key=/etc/ssl/certs/tomaramos.app.key&cert=/etc/ssl/certs/tomaramos.app-bundle.crt&verify_mode=none" \
+        --binding="ssl://${WEB_SERVER_HOST}:${WEB_SERVER_PORT}?key=/etc/ssl/certs/tomaramos.app.key&cert=/etc/ssl/certs/tomaramos.app.crt&verify_mode=none" \
         --environment=development
 elif [[ "${SERVE_OVER_HTTPS}" == "false" ]]; then
     rails server \
