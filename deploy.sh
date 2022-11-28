9f176af00051fa6127aaf6d25f1547629689c7d6
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-11-28 14:56:04 -0300

    Added log for deployment script
    For eventual debugging

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,28 @@
 # cloned at the production server.
 # --------------------------------------------------------------------------------------------------
 
-set -exu
+set -eu
+
+LOG_FILE="./deploy.log"
+date > ${LOG_FILE} 
+echo "Outputting to ${LOG_FILE}"
+
+set -x
 
 cd ${TOMARAMOSAPP_RAILS}
 
-git pull origin master
-git fetch && git status
+git pull origin master | tee -a ${LOG_FILE}
+git fetch | tee -a ${LOG_FILE}
+git status | tee -a ${LOG_FILE}
 
 # Launching postgres server if not running
 # References: https://serverfault.com/a/935674
 if [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q tomaramos-postgres)` ]; then
-    docker-compose up --build --detach tomaramos-postgres
+    docker-compose up --build --detach tomaramos-postgres | tee -a ${LOG_FILE}
 fi
 
-./restart-rails.sh
-./restart-html-to-image.sh
+./restart-rails.sh | tee -a ${LOG_FILE}
+./restart-html-to-image.sh | tee -a ${LOG_FILE}
+echo "[OK] Restart done" >> ${LOG_FILE}
 
 docker-compose logs -f tomaramos-rails
