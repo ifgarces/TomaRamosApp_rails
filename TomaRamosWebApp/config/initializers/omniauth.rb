a33ed9a5b3938f79df89e0f54c9fc42b7c67db46
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-12-20 17:39:01 -0300

    Removed unused Figaro gem
    Replaced with safer and simpler `ENV.fetch` build-in method

--- TomaRamosWebApp/config/initializers/omniauth.rb
+++ TomaRamosWebApp/config/initializers/omniauth.rb
@@ -1,17 +1,17 @@
 require "omniauth"
-require "figaro"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   #TODO: assert the ENV variables exist when implementing Oauth
 
   provider :developer if (Rails.env.development?)
+
   provider :google_oauth2,
-           ENV["OAUTH_CLIENT_ID"],
-           ENV["OAUTH_CLIENT_SECRET"],
-           scope: "email,profile",
-           access_type: "online",
-           response_type: "code",
-           redirect_uri: "http://localhost:8888/google-auth/custom-redirect"
+    ENV.fetch("OAUTH_CLIENT_ID") { nil },
+    ENV.fetch("OAUTH_CLIENT_SECRET") { nil },
+    scope: "email,profile",
+    access_type: "online",
+    response_type: "code",
+    redirect_uri: "http://localhost:8888/google-auth/custom-redirect"
 end
 
 OmniAuth.config.allowed_request_methods = %i[get]
