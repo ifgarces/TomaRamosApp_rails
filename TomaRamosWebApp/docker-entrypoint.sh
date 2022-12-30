69c56ae8c75f68eab84b632d40aa16f803c8dd2b
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-12-29 23:43:59 -0300

    WIP #17: added local self-signed SSL
    Added working method for running rails over HTTPS locally for development, with a self-signed SSL cert

--- TomaRamosWebApp/docker-entrypoint.sh
+++ TomaRamosWebApp/docker-entrypoint.sh
@@ -20,11 +20,8 @@ rake data_importer:csv --trace
 
 # Starting web server, logging to STDOUT/STDERR
 if [[ "${SERVE_OVER_HTTPS}" == "true" ]]; then
-    #! UGLY temporal workaround, must properly use production environment instead!
-    sed -i 's/config.consider_all_requests_local = true/config.consider_all_requests_local = false/' ./config/environments/development.rb
-    sed -i 's/# config.force_ssl = true/config.force_ssl = true/' ./config/environments/development.rb
+    #! Local dev SSL certificate
 
-    #! Testing localhost now
     echo "
 [ req ]
 prompt                 = no
@@ -36,8 +33,8 @@ req_extensions         = v3_req
 countryName            = CL
 stateOrProvinceName    = Metropolitana
 localityName           = Santiago
-organizationName       = tomaramos.app
-organizationalUnitName = ifgarces
+organizationName       = localhost
+organizationalUnitName = R&D
 commonName             = localhost
 emailAddress           = hostmaster@tomaramos.app
 
@@ -49,20 +46,23 @@ subjectAltName         = @sans
 [ sans ]
 DNS.0 = localhost
 DNS.1 = localhost
-    " >> ca-config.temp.conf
+    " >> /tmp/local-ca-config.conf
+
+    SSL_KEY_PATH=/tmp/localhost.key
+    SSL_CERT_PATH=/tmp/localhost.crt
 
     # Creating self-signed SSL certificate
     openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -utf8 \
-        -keyout /etc/ssl/certs/tomaramos.app.key \
-        -out /etc/ssl/certs/tomaramos.app.crt \
-        -config ca-config.temp.conf
-        #"/C=CL/ST=Región Metropolitana/L=Santiago/O=TomaRamosApp/OU=/CN=/emailAddress="
+        -keyout ${SSL_KEY_PATH} \
+        -out ${SSL_CERT_PATH} \
+        -config /tmp/local-ca-config.conf
 
     # References: https://stackoverflow.com/a/31939157/12684271
     echo "::: SERVING OVER HTTPS :::"
+    FORCE_SSL=1 \
     rails server \
         --using=puma \
-        --binding="ssl://0.0.0.0:${WEB_SERVER_PORT}?key=/etc/ssl/certs/tomaramos.app.key&cert=/etc/ssl/certs/tomaramos.app.crt&verify_mode=none" \
+        --binding="ssl://0.0.0.0:${WEB_SERVER_PORT}?key=${SSL_KEY_PATH}&cert=${SSL_CERT_PATH}&verify_mode=none" \
         --environment=development
 
 elif [[ "${SERVE_OVER_HTTPS}" == "false" ]]; then
